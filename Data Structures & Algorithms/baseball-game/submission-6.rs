187805131e1f834e827725f3e1ee045574c08c49
⟣ €₥ℵ∪ℓ ⟢ <34749913+emnul@users.noreply.github.com>
2026-04-08 11:16:30 -0400

    Add: baseball-game - submission-6

---
+++ Data Structures & Algorithms/baseball-game/submission-6.rs
@@ -0,0 +1,23 @@
+impl Solution {
+    pub fn cal_points(operations: Vec<String>) -> i32 {
+        let mut record = vec![];
+        for op in operations {
+            if let Ok(r) = op.parse::<i32>() {
+                record.push(r);
+            } else if op == "D" {
+                let r = record.pop().unwrap();
+                record.push(r);
+                record.push(r * 2);
+            } else if op == "+" {
+                let r2 = record.pop().unwrap();
+                let r1 = record.pop().unwrap();
+                record.push(r1);
+                record.push(r2);
+                record.push(r1+r2);
+            } else if op == "C" {
+                record.pop();
+            }
+        }
+        record.into_iter().sum()
+    }
+}
