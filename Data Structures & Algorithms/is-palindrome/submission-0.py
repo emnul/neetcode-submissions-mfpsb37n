d9fbf79b68d451d083b9529a84959118b37d787e
⟣ €₥ℵ∪ℓ ⟢ <34749913+emnul@users.noreply.github.com>
2026-07-17 16:43:57 -0400

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        L = 0
+        R = len(s) - 1
+        while L < R:
+            print(s[L], s[R])
+            print(L, R)
+            if (
+                (ord(s[L].lower()) not in range(ord('a'), ord('z') + 1)) and 
+                (ord(s[L]) not in range(ord('0'), ord('9') + 1))
+            ):
+               L += 1
+               continue
+            if (
+                (ord(s[R].lower()) not in range(ord('a'), ord('z') + 1)) and 
+                (ord(s[R]) not in range(ord('0'), ord('9') + 1))
+            ):
+               R -= 1
+               continue
+            
+            if s[L].lower() != s[R].lower():
+                return False
+            
+            L += 1
+            R -= 1
+        return True
