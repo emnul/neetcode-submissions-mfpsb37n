15b7a38671e9ae96c336a5c83fa2d9a67bfd5842
⟣ €₥ℵ∪ℓ ⟢ <34749913+emnul@users.noreply.github.com>
2026-07-21 15:17:48 -0400

    Add: validate-parentheses - submission-24

---
+++ Data Structures & Algorithms/validate-parentheses/submission-24.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        # Mapping from closing bracket to open bracket
+        bracketMap = {"}": "{", "]": "[", ")": "("}
+
+        for char in s:
+            # Add opening brackets to stack
+            if char not in bracketMap:
+                stack.append(char)
+            else:
+                # Check for empty stack
+                if not stack:
+                    return False
+                else:
+                    openB = stack.pop()
+                    if bracketMap[char] != openB:
+                        return False
+        
+        if stack:
+            return False
+        else:
+            return True
+
