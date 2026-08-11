46d6a93f007616ee49290431fc91769d254b6d24
⟣ €₥ℵ∪ℓ ⟢ <34749913+emnul@users.noreply.github.com>
2026-08-11 16:01:51 -0400

    Add: mergeSort - submission-2

---
+++ Data Structures & Algorithms/mergeSort/submission-2.py
@@ -0,0 +1,51 @@
+# Definition for a pair.
+# class Pair:
+#     def __init__(self, key: int, value: str):
+#         self.key = key
+#         self.value = value
+class Solution:
+    def mergeSort(self, pairs: List[Pair]) -> List[Pair]:
+        return self.mergeSortHelper(pairs, 0, len(pairs) - 1)
+    
+    def mergeSortHelper(self, pairs, s, e):
+        if e - s + 1 <= 1:
+            return pairs
+        
+        m = (s + e) // 2
+        self.mergeSortHelper(pairs, s, m)
+        self.mergeSortHelper(pairs, m + 1, e)
+        self.merge(pairs, s, m, e)
+        
+        return pairs
+    
+    def merge(self, pairs, s, m, e):
+        L = pairs[s:m+1]
+        R = pairs[m+1:e + 1]
+
+        i = 0 # l ind
+        j = 0 # r ind
+        k = s # pair ind
+
+        # merge sorted arrs
+        while i < len(L) and j < len(R):
+            if L[i].key <= R[j].key:
+                pairs[k] = L[i]
+                i += 1
+            else:
+                pairs[k] = R[j]
+                j += 1
+            k += 1
+        
+        while i < len(L):
+            pairs[k] = L[i]
+            i += 1
+            k += 1
+        
+        while j < len(R):
+            pairs[k] = R[j]
+            j += 1
+            k += 1
+        
+
+        
+
